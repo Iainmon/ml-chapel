48370cc20eb7defc5087301f691c05040d48b4ea
Iain Moncrief <Iainmoncrief@gmail.com>
2023-09-05 15:48:28 -1000

    running tests

--- week11/run_batch_trainer.bash
+++ week11/run_batch_trainer.bash
@@ -9,7 +9,10 @@ mkdir -p batch_tests/output/chapel
 # Run the python tests
 # for data_size in 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300; do
 for data_size in 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500; do
-    sleep 5 && salloc --nodes=1 --cpus-per-task=64 --partition=chapdl --exclusive srun ./mnist_trainer --dataSize=$data_size > batch_tests/output/chapel/mnist_trainer_$data_size.csv &
+    cat sbatch_header.bash > sbatch_tmp.bash
+    echo "./mnist_trainer --dataSize=$data_size > batch_tests/output/chapel/mnist_trainer_$data_size.csv" >> sbatch_tmp.bash
+    sbatch -W sbatch_tmp.bash &
+    # sleep 5 && salloc --nodes=1 --cpus-per-task=64 --partition=chapdl --exclusive srun ./mnist_trainer --dataSize=$data_size > batch_tests/output/chapel/mnist_trainer_$data_size.csv &
 done
 
 jobs > batch_tests/jobs.txt
---
+++ week11/sbatch_header.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -t 3-8:0
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --partition=chapdl
+#SBATCH --cpus-per-task=64
+
+
+
