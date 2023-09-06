128301c3b1fbcd32b6d2cc62d80647ecb7e2d799
Iain Moncrief <Iainmoncrief@gmail.com>
2023-09-05 14:38:17 -1000

    running tests

--- week11/run_batch_trainer.bash
+++ week11/run_batch_trainer.bash
@@ -8,7 +8,22 @@ mkdir -p batch_tests/output/chapel
 # Run the python tests
 # for data_size in 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300; do
 for data_size in 20 40 60 80 100; do
-    salloc --nodes=1 --partition=chapdl --exclusive srun ./mnist_trainer --dataSize=$data_size >> batch_tests/output/chapel/mnist_trainer_$data_size.csv &
+    sleep 5 && salloc --nodes=1 --partition=chapdl --exclusive srun ./mnist_trainer --dataSize=$data_size >> batch_tests/output/chapel/mnist_trainer_$data_size.csv &
 done
 
 jobs > batch_tests/jobs.txt
+jobs -p > batch_tests/pids.txt
+
+for job in `jobs -p`
+do
+echo $job
+    wait $job
+done
+
+# Agregate data
+for data_size in 20 40 60 80 100; do
+    time_output=$(cat batch_tests/output/chapel/mnist_trainer_$data_size.csv | tail -n 1)
+    num_seconds=$(echo $time_output | awk -F ': ' '{print $2}')
+    echo "$data_size,$num_seconds" >> batch_tests/mnist_trainer.chpl.csv
+done
+
