c58c8541ba0bee2bce6adc337570557fe3bafc81
Iain Moncrief <Iainmoncrief@gmail.com>
2023-09-05 14:17:11 -1000

    testing

---
+++ week11/run_batch_trainer.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p batch_tests
+mkdir -p batch_tests/output
+mkdir -p batch_tests/output/python
+mkdir -p batch_tests/output/chapel
+
+# Run the python tests
+# for data_size in 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300; do
+for data_size in 20 40 60 80 100; do
+    salloc --nodes=1 --partition=chapdl --exclusive srun ./mnist_trainer --dataSize=$data_size >> batch_tests/output/chapel/mnist_trainer_$data_size.csv &
+done
